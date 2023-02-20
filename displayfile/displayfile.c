3623458e15ecaed4a3300f71f920315bf0777933
Preethiveginati <120377958+Preethiveginati@users.noreply.github.com>
2023-02-20 15:18:35 +0530

    Add files via upload

---
+++ displayfile/displayfile.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+FILE *fp;
+char ch;
+fp=fopen("fir.txt","w+");
+if(fp==NULL)
+{
+printf("file cannot be created");
+exit(1);
+}
+else
+{
+while((ch=getchar())!='\n')
+{
+fputc(ch,fp);
+}
+while((ch=fgetc(fp))!=EOF){
+printf("%c",ch);
+}
+}
+fclose(fp);
+return 0;
+}
